1c825a82a0a948e177ab185c6a567571d976d551
Garrett Pace <elderpace6@gmail.com>
2023-04-21 13:58:44 -0600

    progress!

--- home.js
+++ home.js
@@ -21,7 +21,8 @@
     'Welcome back, Andrew'
 */
 
-//CODE HERE
+const greetUser = ((username) => `Welcome back, ${username}`)
+console.log(greetUser("Andrew"))
 
 
 
@@ -29,16 +30,9 @@
 
 //////////////////PROBLEM 2////////////////////
 /* 
-    Below is an array of zip codes that are in
-    the restaurant's delivery zone. 
-
-    Write a function called `canWeDeliver` that
-    takes in one argument, `zipCode`.
-
-    If the zip code passed in is in the array,
-    return a string letting the user know they
-    are eligible for delivery. If they are not, 
-    return a string letting them know that. 
+    Below is an array of zip codes that are in the restaurant's delivery zone. 
+    Write a function called `canWeDeliver` that takes in one argument, `zipCode`.
+    If the zip code passed in is in the array, return a string letting the user know they are eligible for delivery. If they are not, return a string letting them know that. 
 
     For example:
     canWeDeliver(84606) 
@@ -49,7 +43,17 @@
 
 const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
-//CODE HERE
+function canWeDeliver(zipCode) {
+    for (let i=0; i < deliveryAreaZipCodes.length; i++) {
+    if (zipCode === deliveryAreaZipCodes[i]) {
+        return `You're in our delivery zone!`
+    }
+}
+    return `Sorry, we can't deliver to that address`
+}
+console.log(canWeDeliver(85206))
+console.log(canWeDeliver(89121))
+
 
 
 
@@ -57,20 +61,21 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
     Problem 2 Continued
 
     Now you're going to rewrite your function.
-
-    If you wrote `canWeDeliver` using a loop of
-    some kind, write a new function (`canWeDeliverTwo`)
-    below, using the `includes` array method. 
-    Look it up on MDN if you're not sure how to use 
-    it. 
-
-    If you already used the `includes` method, 
-    write a new function using some sort of 
-    loop (for loop, higher order array method).
+    If you wrote `canWeDeliver` using a loop of some kind, write a new function (`canWeDeliverTwo`) below, using the `includes` array method. 
+    Look it up on MDN if you're not sure how to use it. 
+    If you already used the `includes` method, write a new function using some sort of loop (for loop, higher order array method).
     Name your new function `canWeDeliverTwo`.
 */
 
-// CODE HERE
+function canWeDeliver2(zipCode) {
+    if (deliveryAreaZipCodes.includes(zipCode)) {
+        return `You're in our delivery zone!`
+} else {
+    return `Sorry, we can't deliver to that address`
+}
+}
+console.log(canWeDeliver(85206))
+console.log(canWeDeliver(89121))
 
 
 //////////////////PROBLEM 3////////////////////
