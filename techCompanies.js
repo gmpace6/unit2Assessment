a5b80ab3cc6e952c671092d10af9cf3de19ee53f
Garrett Pace <elderpace6@gmail.com>
2023-04-21 20:13:56 -0600

    techCompaniesAdded!

---
+++ techCompanies.js
@@ -0,0 +1,28 @@
+// Technology Stack info courtesy of https://6sense.com/company
+
+let techCompanies = [
+    tc1 = {
+     name: 'nvidia',
+     founded: 1993,
+     glassDoorRating: 4.7,
+     RevenueInBillions: 26.91,
+     programmingLanguagesAndFrameworks: ["MS Azure", "JS", "Golang", "Datastax", "Kubernetes-native", "Adobe Air", "...and many others"]
+ },
+    tc2 = {
+     name: 'google',
+     founded: 1998,
+     glassDoorRating: 4.4,
+     RevenueInBillions: 279.8,
+     programmingLanguagesAndFrameworks: ["Apache Qpid", "Golang", "JS", "Google Cloud Search", "Kubernetes-native", "Google Diagflow", "...and many others"]
+},
+    tc3 = {
+     name: 'adobe',
+     founded: 1982,
+     glassDoorRating: 4.4,
+     RevenueInBillions: 17,
+     programmingLanguagesAndFrameworks: ["Apache Qpid", "IBM Rational Build Forge", "JS", "AWS Transfer for SFTP", "Golang", "Google Cloud Search", "...and many others"]
+},
+]
+
+console.log(techCompanies)
+console.table(techCompanies)
